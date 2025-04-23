0d05deccf5248f1f9f7753272cecda33b11f2b23
Aliaa <aliaaalihesh@gmail.com>
2025-04-24 01:11:44 +0200

    Product Crud Done for all requirment

--- dashboard/productCrud/products.php
+++ dashboard/productCrud/products.php
@@ -3,7 +3,7 @@
     if (!$myConnection) {
       die("Database connection failed");
   }
-  $items_per_page = 4; 
+  $items_per_page = 3; 
   $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
   if ($current_page < 1) $current_page = 1;
   $offset = ($current_page - 1) * $items_per_page;
@@ -45,10 +45,10 @@
                         <button class="btn btn-primary" type="submit">Search</button>
                     </div>
                 </div>
-                <button class="btn btn-success" type="button" data-bs-toggle="modal" data-bs-target="#addProductModal">
-                    <i class="fas fa-plus me-2"></i>Add Product
-                </button>
             </div>
+            <button class="btn btn-success mt-4" type="button" data-bs-toggle="modal" data-bs-target="#addProductModal">
+                    <i class="fas fa-plus me-2"></i>Add Product
+            </button>
             <input type="hidden" name="page" value="1">
         </form>
         <!-- display items -->
